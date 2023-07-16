917fe981f211baf42e80ce0368b9cefe60370854
Taniya <tanya.kalra200@gmail.com>
2023-07-16 17:29:08 +0530

    submitting code-Sun Jul 16 2023 17:29:05 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function validate(){
+    if(name && password){
+      setName("")
+      setPassword("")
+      setIsLoggedIn(true);
+      return;
+    }
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="parent">
+          <h1>Parent Component</h1>
+          <div className="child">
+              <label htmlFor="name">Username: </label>
+              <input type="text" id="name" onChange={(e)=>setName(e.target.value)} value={name}/>
+              <br/><br/>
+              <label htmlFor="password">Password: </label>
+              <input type="password" id="password" onChange={(e)=>setPassword(e.target.value)} value={password}/>
+              <br/><br/>
+              <button type="submit" onClick={validate}>Login</button>
+          </div>
+        </div>
     </div>
   )
 }
